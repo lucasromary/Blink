a18ae7518c7c39490f672168dc18e6e570a05c38
Lucas ROMARY <lucas.romary@utbm.fr>
2022-01-10 10:11:23 +0100

    ESP8266 Feather test

---
+++ src/main.cpp
@@ -0,0 +1,41 @@
+#include <Arduino.h>
+//#include <WiFi.h>
+#include <ESP8266WiFi.h> 
+
+const char* ssid     = "Crunch LAB";
+const char* password = "90xV@FsT";
+
+void setup() {
+  // initialize digital pin LED_BUILTIN as an output.
+  pinMode(LED_BUILTIN, OUTPUT);
+   Serial.begin(115200);
+    delay(10);
+
+    // We start by connecting to a WiFi network
+
+    Serial.println();
+    Serial.println();
+    Serial.print("Connecting to ");
+    Serial.println(ssid);
+
+    WiFi.begin(ssid, password);
+
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print(".");
+    }
+
+    Serial.println("");
+    Serial.println("WiFi connected");
+    Serial.println("IP address: ");
+    Serial.println(WiFi.localIP());
+
+}
+
+// the loop function runs over and over again forever
+void loop() {
+  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(1000);                       // wait for a second
+  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+  delay(1000);                       // wait for a second
+}
